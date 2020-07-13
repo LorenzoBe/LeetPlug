0deb6cc20c3266bbf8b5d5cdc3f3b6c820ce49e8
Lorenzo Bertelli <lorenzo.bertelli@hexagon.com>
2020-07-13 21:01:36 +0200

    Add first try for user creation

--- webapp/application.py
+++ webapp/application.py
@@ -4,6 +4,10 @@ from flask import render_template
 from flask import request
 from flask_httpauth import HTTPBasicAuth
 from werkzeug.security import generate_password_hash, check_password_hash
+import uuid
+
+from storage import Storage
+from gmailHelper import EmailHelper
 
 app = Flask(__name__)
 auth = HTTPBasicAuth()
@@ -14,15 +18,48 @@ users = {
     config['WebApp']['AdminUsername']: generate_password_hash(config['WebApp']['AdminPassword']),
 }
 
+storage = Storage(config)
+emailHelper = EmailHelper(config)
+
 @auth.verify_password
 def verify_password(username, password):
     if username in users and \
             check_password_hash(users.get(username), password):
         return username
 
+@app.route('/users', methods = ['PUT'])
+@auth.login_required
+def usersFunction():
+    if request.method == 'PUT':
+        email = request.args.get('email', default='', type = str)
+
+        # TODO: add a better validation here
+        if email == '': return 'Request failed. Email missing.', 500
+
+        # generate the user incremental id
+        userId = storage.getNextCounter('userIdCounter')
+        if userId < 0: return 'Request failed. UserId invalid.', 500
+
+        # generate the user key
+        userKey = str(uuid.uuid4())
+
+        newUser = {
+            'id': email,
+            'userId': userId,
+            'key': userKey
+        }
+
+        if not storage.upsertUser(newUser):
+            return 'Request failed. Insert failed.', 500
+
+        emailHelper.send(email, 'Account activated', 'UserID: {}\nUserKey: {}'.format(userId, userKey), "<b>Enjoy!</b>")
+        return 'Request succeded.', 202
+
+    return 'Request unsupported.', 500
+
 @app.route('/events', methods = ['PUT'])
 @auth.login_required
-def events():
+def eventsFunction():
     if request.method == 'PUT':
         return "ECHO: PUT\n"
     
